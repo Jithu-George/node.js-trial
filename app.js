9434011b789fe63a919305d9f53961a89e53c72a
Jithu george <jithugeorge20@gmail.com>
2024-01-24 13:55:17 +0530

    learning about ejx

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express= require('express');
 
+
 //express app
 const app=express();
+
+//register view engine
+app.set('view engine','ejs');
 
 //listening for request
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.sendFile('./index.html',{root:__dirname});
+    res.render('index');
 });
 
